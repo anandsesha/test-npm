869f82bbe08e4c36e103b2f4e458af81fa66d6fa
anandsesha <anandseshawork@gmail.com>
2023-07-31 20:59:51 -0400

    moment

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ console.log(moment().format('MMMM Do YYYY'));
 console.log(moment.isDate('2020-01-01'));
 console.log(moment.isDate(moment().format('MMMM Do YYYY')));
 
+console.log(moment().isValid('2020-01-01'))
+console.log(moment().isValid('2020-14-01'))
+console.log(moment().isValid('01-01'))
+console.log(moment().calendar())
+
+// Add 7 days to the current date
+console.log(moment().add(7,'days'))
+
+
 
 let chunkOfArray = loadash.chunk(["Anand","Arvind","Amma","Appa"],2);
 console.log(chunkOfArray); 
